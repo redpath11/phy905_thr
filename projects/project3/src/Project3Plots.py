edf435197f987f97272f93cb3155bfe4a9a9d828
redpath <redpath@nscl.msu.edu>
2017-03-15 17:08:55 -0400

    Euler and Verlet 2body codes; working on 3body

--- projects/project3/src/Project3Plots.py
+++ projects/project3/src/Project3Plots.py
@@ -3,10 +3,16 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 filein = "../Benchmark/SEsystemEuler.out"
+fileV  = "../Benchmark/SEsystemVerlet.out"
 
 data = np.loadtxt(filein)
 x = data[0,:]
 y = data[1,:]
 
-plot = plt.plot(x,y)
+dataV = np.loadtxt(fileV)
+xV = dataV[0,:]
+yV = dataV[1,:]
+
+plotE = plt.plot(x,y,'b')
+plotV = plt.plot(xV,yV,'r')
 plt.show()
--- projects/project3/src/proj3.cc
+++ projects/project3/src/proj3.cc
@@ -12,6 +12,7 @@ const double GM = 4.*3.14159265*3.14159265;
 
 /* Function Declarations */
 void Euler(int n,double years,double **);
+void Verlet(int n,double years,double **);
 void SunEarth();
 int main();
 
@@ -33,21 +34,27 @@ void SunEarth()
   int n=100; double yr = 1.;
   // initial positions and velocities
   double **PV = AllocateMatrix(4,n);
-  PV[0][0]=1.; PV[1][0] = 0.;
-  PV[2][0] = 365.25*(-3.158e-3); PV[3][0] = 365.25*(-1.701e-2);//[AU/yr]
+  double **VV = AllocateMatrix(4,n);
+//  PV[0][0]=VV[0][0]=1.; PV[1][0]=VV[1][0] = 0.;
+//  PV[2][0]=VV[2][0] = 365.25*(-3.158e-3); PV[3][0]=VV[3][0] = 365.25*(-1.701e-2);//[AU/yr]
+  PV[0][0]=VV[0][0]=0.; PV[1][0]=VV[1][0] = 1.;
+  PV[2][0]=VV[2][0] = 365.25*(-0.017301); PV[3][0]=VV[3][0] = 365.25*(0.);//[AU/yr]
 
   Euler(n,yr,PV);
+  Verlet(n,yr,VV);
 
   FileMatrix("../Benchmark/SEsystemEuler.out",PV,4,n);
+  FileMatrix("../Benchmark/SEsystemVerlet.out",VV,4,n);
 
   DeallocateMatrix(PV,4,n);
+  DeallocateMatrix(VV,4,n);
 
 }
 
 void Euler(int n,double years,double ** xyvxvy)
 {
 /* The matrix xyvxvy will hold the positons and velocities
- * at each time step with the following convention:
+ * at each time step (i) with the following convention:
  * x -> xyvxvy[0][i]
  * y -> xyvxvy[1][i]
  * vx-> xyvxvy[2][i]
@@ -63,6 +70,77 @@ void Euler(int n,double years,double ** xyvxvy)
     xyvxvy[1][i+1] = xyvxvy[1][i] + xyvxvy[3][i]*h;
     xyvxvy[2][i+1] = xyvxvy[2][i] - NumeratorCoefficient*xyvxvy[0][i]/ ri3;
     xyvxvy[3][i+1] = xyvxvy[3][i] - NumeratorCoefficient*xyvxvy[1][i]/ ri3;
+  }
+
+}
+
+void ThreeBodyEuler(int n,double years,double ** xyvxvy)
+{
+/* The matrix xyvxvy will hold the positons and velocities
+ * at each time step (i) with the following convention:
+ * ----- Earth -----
+ * x -> xyvxvy[0][i]
+ * y -> xyvxvy[1][i]
+ * vx-> xyvxvy[2][i]
+ * vy-> xyvxvy[3][i]
+ * ---- Jupiter ----
+ * x -> xyvxvy[4][i]
+ * y -> xyvxvy[5][i]
+ * vx-> xyvxvy[6][i]
+ * vy-> xyvxvy[7][i]
+*/
+  double h = years/((double) n);
+  double NumeratorCoefficient = GM*h;
+  double MJMSunRatio = 9.5458e-4; double MEMSunRatio = 1.0e-6;
+  for(int i=0;i<n-1;i++)
+  {
+    // Earth-Sun distance
+    double ri3 = sqrt(xyvxvy[0][i]*xyvxvy[0][i] + xyvxvy[1][i]*xyvxvy[1][i]);
+    ri3 = ri3*ri3*ri3;
+    double rJi3 = sqrt(xyvxvy[4][i]*xyvxvy[4][i] + xyvxvy[5][i]*xyvxvy[5][i]);
+    rJi3 = rJi3*rJi3*rJi3;
+    // Earth-Jupiter distance
+    double rEJx = xyvxvy[0][i] - xyvxvy[4][i];
+    double rEJy = xyvxvy[1][i] - xyvxvy[5][i];
+    double rEJ3 = sqrt(rEJx*rEJx + rEJy*rEJy);
+    rEJ3 = rEJ3*rEJ3*rEJ3;
+    xyvxvy[0][i+1] = xyvxvy[0][i] + xyvxvy[2][i]*h;
+    xyvxvy[1][i+1] = xyvxvy[1][i] + xyvxvy[3][i]*h;
+    xyvxvy[2][i+1] = xyvxvy[2][i] - NumeratorCoefficient*(xyvxvy[0][i]/ ri3 + MJMSunRatio*rEJx/rEJ3);
+    xyvxvy[3][i+1] = xyvxvy[3][i] - NumeratorCoefficient*(xyvxvy[1][i]/ ri3 + MJMSunRatio*rEJy/rEJ3);
+
+    xyvxvy[4][i+1] = xyvxvy[4][i] + xyvxvy[6][i]*h;
+    xyvxvy[5][i+1] = xyvxvy[5][i] + xyvxvy[7][i]*h;
+    xyvxvy[6][i+1] = xyvxvy[6][i] - NumeratorCoefficient*(xyvxvy[4][i]/rJi3 + MEMSunRatio*rEJx/rEJ3);
+    xyvxvy[7][i+1] = xyvxvy[7][i] - NumeratorCoefficient*(xyvxvy[5][i]/rJi3 + MEMSunRatio*rEJx/rEJ3);
+  }
+
+}
+
+void Verlet(int n,double years,double ** xyvxvy)
+{
+/* The matrix xyvxvy will hold the positons and velocities
+ * at each time step (i) with the following convention:
+ * x -> xyvxvy[0][i]
+ * y -> xyvxvy[1][i]
+ * vx-> xyvxvy[2][i]
+ * vy-> xyvxvy[3][i]
+*/
+  double h = years/((double) n);
+  double hGMOver2 = h*GM/2.; double hhGMOver2 = h*h*GM/2.;
+  for(int i=0;i<n-1;i++)
+  {
+    double ri  = sqrt(xyvxvy[0][i]*xyvxvy[0][i] + xyvxvy[1][i]*xyvxvy[1][i]);
+    double xri = xyvxvy[0][i] / ri;
+    double yri = xyvxvy[1][i] / ri;
+    // positions
+    xyvxvy[0][i+1] = xyvxvy[0][i] + h*xyvxvy[2][i] - hhGMOver2*xri;// should be minus on last term??
+    xyvxvy[1][i+1] = xyvxvy[1][i] + h*xyvxvy[3][i] - hhGMOver2*yri;// should be minus on last term??
+    double rii = sqrt(xyvxvy[0][i+1]*xyvxvy[0][i+1] + xyvxvy[1][i+1]*xyvxvy[1][i+1]);
+    double xrii = xyvxvy[0][i+1] / rii;
+    double yrii = xyvxvy[1][i+1] / rii;
+    xyvxvy[2][i+1] = xyvxvy[2][i] - hGMOver2*(xrii + xri);// should be minus on last term??
+    xyvxvy[3][i+1] = xyvxvy[3][i] - hGMOver2*(yrii + yri);// should be minus on last term??
   }
 
 }
